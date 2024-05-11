40be7b40f7ed61ec14823f16f3d002cbfa2f9d2c
Jegor <140427023+jegorRegularUser@users.noreply.github.com>
2024-05-11 14:28:07 +0300

    исправление ошибок
    На странице входа и регистрации сообщения об ошибках выводились при помощи алертов.
    Добавлены подробности ошибок
    Ошибки при работе с кошельком выводятся под операциями с ним

--- src/CartStore/CartContextProvider.js
+++ src/CartStore/CartContextProvider.js
@@ -1,10 +1,37 @@
 import React, { useState } from 'react'
-import {
-	dishCapital,
-	dishOrientalBuzz,
-	dishRegularWithChicken,
-} from '../DishesDescriptionList'
+
 import CartContext from './CartContext'
+
+class Dish {
+	constructor(id, name, description, price, isInCart) {
+		this.id = id
+		this.name = name
+		this.description = description
+		this.price = price
+		this.isInCart = isInCart
+	}
+}
+const dishCapital = new Dish(
+	0,
+	'Capital',
+	'potato, chicken fillet, pickled cucumber, tomato, cucumber, spicy sauce',
+	2.99,
+	false
+)
+const dishRegularWithChicken = new Dish(
+	1,
+	'Regular with chicken',
+	'chicken fillet, tomato, cucumber, signature sauce',
+	1.99,
+	false
+)
+const dishOrientalBuzz = new Dish(
+	2,
+	'Oriental buzz',
+	'satsebeli, cilantro, bell pepper, cucumber, tomato, soy-ginger sauce',
+	0.99,
+	false
+)
 
 const reducer = (state, action) => {
 	const isInCartState = state[state.action].isInCart
